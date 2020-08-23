1c3952466789f05fae72f8bdc6e433484a0cc9f9
phmiranda <pehhagah.1607@gmail.com>
2020-08-23 18:50:19 -0300

    first commit

---
+++ receitas/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+    receitas = {
+        1: 'Lasanha',
+        2: 'Sopa de Legumes',
+        3: 'Sorvete',
+        4: 'Bolo de Chocolate'
+    }
+
+    dados = {
+        'nome_das_receitas' : receitas
+    }
+    return render(request, 'index.html', dados)
+
+
+def receitas(request):
+    return render(request, 'receita.html')
