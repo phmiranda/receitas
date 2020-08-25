4ff08debfd5058891c358b2d7958d7b95eb2f10b
phmiranda <pehhagah.1607@gmail.com>
2020-08-25 17:35:54 -0300

    [add] - modificações conforme a quinta aula, onde é feito a ativação do admin.

--- setup/settings.py
+++ setup/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db_receitas.sqlite3'),
+        'ENGINE': os.environ.get('DB_ENGINE'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USERNAME'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'Host': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT')
     }
 }
 
