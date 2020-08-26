6ec4a5f6c375903a7af74ba406e8b335e83516a4
phmiranda <pehhagah.1607@gmail.com>
2020-08-26 08:41:38 -0300

    [add] - finalização da primeira parte do curso de django framework.

--- receitas/urls.py
+++ receitas/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('receita', views.receitas, name='receita')
+    path('<int:receita_id>', views.receita, name='receita')
 ]
